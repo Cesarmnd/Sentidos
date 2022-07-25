640db6901e33828e822c63f787d1e0428e1193a1
Cesarmnd <cesarn.davila@gmail.com>
2022-07-25 13:25:27 -0500

    Agregado de boton de Inscripcion

--- scripts/app.js
+++ scripts/app.js
@@ -21,7 +21,7 @@ const redes = [
   }
 ];
 
-// Contenedores
+// Contenedores de Redes del footer
 const filter = document.getElementById('filter');
 const redContenedor = document.querySelector('.redes__iconos-contenedor');
 
@@ -36,24 +36,48 @@ const swalWithBootstrapButtons = Swal.mixin({
   buttonsStyling: false,
 });
 
+// Funcion de Arleta
+function alerta ( btn, link) {
+  swalWithBootstrapButtons.fire({ //btn tomará el valor 1 o 2 para poder definir la posicion de la alerta
+    title: 'SALIENDO DE LA PÁGINA',
+    text: 'Te estaremos redirigiendo a una página externa',
+    showConfirmButton: true,
+    confirmButtonText: `Continuar`,
+    position: btn === 1 ? 'bottom-center' : 'bottom-end',
+  })
+  .then((result) => {
+    if (result.isConfirmed) {
+      window.open(link);
+    };
+  });
+};
+
+// Asignacion de eventos a botones Inscripcion y Calendario
+document.getElementById('inscripcion') != null && inscripcionBtn();
+document.getElementById('calendario') != null && calendarioBtn();
+
+// Evento del boton Inscripcion
+function inscripcionBtn () {
+  const inscripcion = document.getElementById('inscripcion');
+  inscripcion.addEventListener( 'click', () => {
+    alerta( 1, 'https://wa.me/541123925341');
+  })
+};
+
+// Evento del boton Calendario
+function calendarioBtn () {
+  const calendario = document.getElementById('calendario');
+  calendario.addEventListener( 'click', () => {
+    alerta( 1, 'https://www.canva.com/design/DAFG3xydAcc/q_Zt_vok_iHlm2j41W4T3Q/view?utm_content=DAFG3xydAcc&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink')
+  });
+};
+
 // Creación de íconos de redes
 redes.forEach( item => {
   const red = document.createElement('a');
   red.className = 'redes__iconos';
   red.addEventListener('click', () => {
-    
-    swalWithBootstrapButtons.fire({
-      title: 'SALIENDO DE LA PÁGINA',
-      text: 'Te estaremos redirigiendo a una página externa',
-      showConfirmButton: true,
-      confirmButtonText: `Continuar`,
-      position: 'bottom-end',
-    }).then((result) => {
-      if (result.isConfirmed) {
-        // window.location = item.link;
-        window.open(item.link)
-      }
-    })
+    alerta( 2, item.link);
   });
 
   const icono = document.createElement('i');
@@ -61,7 +85,7 @@ redes.forEach( item => {
 
   red.appendChild(icono);
   redContenedor.appendChild(red);
-})
+});
 
 //FOOTER
 const myModalFooter = new bootstrap.Modal(document.getElementById('myModalFooter'))
@@ -84,26 +108,31 @@ async function handleSubmitFooter(event){
   }
 }
 
-//FORMULARIO
+// Activación de la funcion de envío formulario
+document.getElementById('modalContact') != null && enviarForm();
 
-const myModal = new bootstrap.Modal(document.getElementById('modalContact'))
-const $form = document.querySelector("#formContact")
-$form.addEventListener('submit', handleSubmit)
-async function handleSubmit(event){
-  event.preventDefault()
-  const form = new FormData(this)
-  const response = await fetch(this.action, {
-    method: this.method,
-    body: form,
-    headers: {
-      'Accept': 'application/json'
+//FORMULARIO
+function enviarForm () {
+  const myModal = new bootstrap.Modal(document.getElementById('modalContact'))
+  const $form = document.querySelector("#formContact")
+  $form.addEventListener('submit', handleSubmit)
+  async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+      method: this.method,
+      body: form,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    if (response.ok){
+      this.reset()
+      myModal.show()
+      console.log("enviado")
     }
-  })
-  if (response.ok){
-    this.reset()
-    myModal.show()
-    console.log("enviado")
   }
-}
+};
+
 
 
